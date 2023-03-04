97fab68fb5504735c44141ee2129ee5e379992a4
RebeckaNigro <rebeckanigro03@gmail.com>
2023-03-04 14:27:59 -0400

    ProvaDw

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+console.log('teste');
+$(".input-field").on("focus", function() {
+    if ($(".border-danger").not(this).length){
+        $(".border-danger").removeClass("border-danger");
+    }
+    $(this).next().toggleClass("border-danger");
+})
+
